9090149b91eff4a9ed07b81c6382778c0fe387d1
Marc Stoffels <marc@distridata.nl>
2019-02-21 22:56:52 +0100

    Second part of the first assignment

--- captcha.php
+++ captcha.php
@@ -8,7 +8,11 @@
 
   <body>
     <?php
-    $array = [9, 1, 2,1,2,1,2,9];
+    //First part:
+    $array = [1, 1, 1, 1];
+    $arra2 = [1, 1, 1, 1];
+    $arra3 = [1, 2, 3, 4];
+    $arra4 = [9, 1, 2, 1, 2, 1, 2, 9];
     getSum($array);
     function getSum($array){
       $length = count($array);
@@ -23,6 +27,35 @@
         }
       }
       echo "The sum is: " .$sum;
+    }
+    echo "</br>";
+
+    //Second part:
+    $array =  [1, 2, 1, 2];
+    $array2 = [1, 2, 2, 1];
+    $array3 = [1, 2, 3, 4, 2, 5];
+    $array4 = [1, 2, 3, 1, 2, 3];
+    $array5 = [1, 2, 1, 3, 1, 4, 1, 5];
+    getSumHalfway($array5);
+
+    function getSumHalfway($array){
+      $arrayLength = count($array);
+      if($arrayLength % 2 == 0){
+        $sum = 0;
+        $jumps = $arrayLength / 2;
+        for($i = 0; $i < $arrayLength; $i++){
+          if($i >= $jumps){
+            if($array[$i] == $array[$i % $jumps]){
+              $sum += $array[$i];
+            }
+          }else if($array[$i] == $array[$i + $jumps]){
+            $sum += $array[$i];
+          }
+        }
+        echo "The sum is: " .$sum;
+      }else{
+        echo "It is not possible to divide this array into two";
+      }
     }
     ?>
   </body>
